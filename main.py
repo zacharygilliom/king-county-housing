2407fdaa17af17449050a361c53675eda9a6432d
zacharygilliom <zacharygilliom@gmail.com>
2020-03-08 21:02:56 -0400

    added  scatterplots to show correlations between square footage and price. And added a barplot to look at how the number of bedrooms impact the price.

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 import pandas as pd 
 import seaborn as sns 
 import matplotlib.pyplot as plt 
+import numpy as np 
+
+
+sns.set_palette('Blues_d')
+sns.set_style('darkgrid')
+
 df = pd.read_csv('data/kc_house_data.csv')
 
 def change_datetime(df, col):
-	for index, row in df.iterrows():
-		df.loc[index, col] = df.loc[index, col].replace('T', '')
-		df.loc[index, col] = df.loc[index, col][:-6]
-
-	df[col]= df[col].astype(int)
+	df[col] = df[col].apply(lambda x: x[:-7]).astype(int)
 	df[col] = df[col].apply(lambda x: pd.to_datetime(x, format='%Y%m%d'))
 	return df[col]
 
 
-print(df.head())
-
-print(df.dtypes)
-
-print(df['date'])
-
 # Change date col from string to date time format.
 df['date'] = change_datetime(df, 'date')
 
-print(df['date'])
+pal=sns.color_palette('Blues_d', len(list(df['bedrooms'].unique())))
 
+# Barplot fig 1
+fig1, ax = plt.subplots(nrows=1)
+sns.barplot(x='bedrooms', y='price', ci=None, data=df, palette=np.array(pal[::-1]))
+fig1.show()
+
+# scatter plot fig 2
 fig2, ax = plt.subplots(nrows=1)
+sns.scatterplot(x='sqft_living15', y='price', data=df, ax=ax)
+fig2.show()
 
-sns.scatterplot(x='date', y='price', data=df, ax=ax)
-plt.xlim(pd.to_datetime('2014-05-01'),pd.to_datetime('2015-05-01'))
+# scatter plot fig 3
+fig3, ax = plt.subplots(nrows=1)
+sns.scatterplot(x='sqft_lot15', y='price', data=df, ax=ax)
 plt.show()
+
+input('Press Enter to Stop...')
